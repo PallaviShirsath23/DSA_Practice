57ee6846a43f578a47642f4a93831d36e9f6eff7
pallavi23102003 <120883959+pallavi23102003@users.noreply.github.com>
2025-01-06 23:33:49 +0530

    PracticeCode2

---
+++ practicecode2.py
@@ -0,0 +1,7 @@
+Name= input("Enter your name:")
+
+lengthN = len(Name)
+print("The length of your name is:",lengthN)
+
+str = "The company's revenue projections for the upcoming quarter, which were initially set at $10 million, were revised to $12.5 million due to a sudden increase in demand for their products, coupled with a favorable exchange rate that strengthened the $ against other major currencies, ultimately boosting their profit margins significantly"
+print(str.count("$"))
