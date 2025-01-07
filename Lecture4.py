2fcc51b5f7d0e681a4e150b79c3e6cacede952e7
pallavi23102003 <120883959+pallavi23102003@users.noreply.github.com>
2025-01-07 20:38:32 +0530

    Practice

---
+++ Lecture4.py
@@ -0,0 +1,28 @@
+# info = {
+#     " keys" : "value",
+#     "name" : "apnacollege",
+#     " learning" : "coding",
+#     "age" : 35,
+#     "is_adult" : True,
+#     "marks" :94.411
+# }
+
+# print (info)
+# print(type(info))
+
+# print(info["age"])
+
+# info["age"] = 22
+# print(info)
+
+# print(len(list(info.keys())))
+
+# print(info.items())
+
+# print(info["name"])
+# print(info.get("name"))
+
+# info.update({"city" : "pune"})
+# print(info)
+
+ 
