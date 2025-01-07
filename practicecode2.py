15288bddb189a140f6ad8911a1bb70d21be6a0d6
pallavi23102003 <120883959+pallavi23102003@users.noreply.github.com>
2025-01-07 18:30:51 +0530

    Practice

--- practicecode2.py
+++ practicecode2.py
@@ -1,7 +1,33 @@
-Name= input("Enter your name:")
+# Name= input("Enter your name:")
 
-lengthN = len(Name)
-print("The length of your name is:",lengthN)
+# lengthN = len(Name)
+# print("The length of your name is:",lengthN)
 
-str = "The company's revenue projections for the upcoming quarter, which were initially set at $10 million, were revised to $12.5 million due to a sudden increase in demand for their products, coupled with a favorable exchange rate that strengthened the $ against other major currencies, ultimately boosting their profit margins significantly"
-print(str.count("$"))
+# str = "The company's revenue projections for the upcoming quarter, which were initially set at $10 million, were revised to $12.5 million due to a sudden increase in demand for their products, coupled with a favorable exchange rate that strengthened the $ against other major currencies, ultimately boosting their profit margins significantly"
+# print(str.count("$"))"""
+
+# #odd or even
+# Number = int(input("Enter a number:"))
+
+# if (Number % 2 ==0):
+#     print("The number is even")
+# else:
+#     print("The number is odd.")
+    
+# a =int(input("Enter the first number:"))
+# b =int(input("Enter the second number:"))
+# c =int(input("Enter the third number:"))
+
+# if(a>b>c):
+#     print("A is greatest")
+# elif(b>c>a):
+#     print("B is greatest.")
+# else:
+#     print("C is greatest.")
+
+num = int(input("Enter a number:"))
+
+if (num % 7 == 0):
+    print("the number is divisible by 7.")
+else:
+    print ("the number is not divisible by 7.")
