5b130115bf5e1d68301569828cca827d1dcc72e7
pallavi23102003 <120883959+pallavi23102003@users.noreply.github.com>
2025-01-14 23:12:06 +0530

    updated

--- loops.py
+++ loops.py
@@ -117,3 +117,41 @@
 # for i in range (1,11):
 #     print(i*n)
 
+# for i in range(5):
+#     pass
+
+#sum of first n numbers
+
+# n = int(input("Enter a number:"))
+
+# # sum = 0
+# # for i in range (1, n+1):
+# #     sum += i
+# # print(sum)
+
+    #  sum += i
+#     i +=1
+# print(sum) 
+
+# i = 0  
+# sum = 0
+# while i<= n:
+#     sum += i
+#     i +=1
+# print(sum) 
+
+
+#factorial of a number
+# n = 5
+# fact = 1
+# i= 1
+# while i<= n:
+#     fact *= i
+#     i +=1
+# print(fact) 
+
+n = 4
+fact =1 
+for i in range (1, n+1):
+    fact *= i
+print(fact)
