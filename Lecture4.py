d647b521d87bce208167d1e526f3ddb28706dd15
pallavi23102003 <120883959+pallavi23102003@users.noreply.github.com>
2025-01-14 13:54:17 +0530

    updated

--- Lecture4.py
+++ Lecture4.py
@@ -25,4 +25,52 @@
 # info.update({"city" : "pune"})
 # print(info)
 
+# collection = {1,2,2,2,"world", "hello"}
+
+# collection.add(3)
+
+# collection.remove(2)
+
+# print(collection)
+# print (len(collection))
+
+# collection.pop()
+# print(collection)
+
+# collection.clear()
+# print(collection) 
+
+# items = {
+#     "cat":"a small animal",
+#     "table" : ("a piece of furniture" , "list of facts and figures")
+# }
+
+# print(items)
+
+# set = {"python", "java", "C++", "python", "javascript","java", "python", "java", "C++", "C"}
+# print(len(set))
+# print(type(set))
+
+# dict = {}
+
+# x = int(input("Enter the marks of obtained in science:"))
+# y = int(input("Enter the marks of obtained in math:"))
+# z = int(input("Enter the marks of obtained in english:"))
+
+# dict.update({"Science" : x})
+# dict.update({"Math" : y})
+# dict.update({"English" : z})
+
+# print(dict)
+
+# num1 = int(input("Enter a number:"))
+# num2 = float(input("Enter a number in decimal:"))
+
+values = {
+    ("float",9.0),
+    ("int",9)
+}
+print(values)
+
  
+
