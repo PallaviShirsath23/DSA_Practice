9f966e0e323b05b42e422d21b6c1af21bedaddb5
pallavi23102003 <120883959+pallavi23102003@users.noreply.github.com>
2025-01-14 22:29:35 +0530

    new

--- loops.py
+++ loops.py
@@ -34,4 +34,66 @@
 # while i <len(nums):
 #     if(nums[i] == x):
 #         print("Found at index:", i)
+#         break 
+#     else:
+#         print(Finding....)
+
 #     i += 1
+    
+    
+    
+    
+
+# i= 1
+# while i<=5 :
+#     print(i)
+#     if(i == 3):
+#         break
+#     i += 1
+
+# i= 1
+# while i<=5 :
+#     if(i == 3):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+# i= 1
+# while i<=10 :
+#     if(i%2 == 0):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+
+# nums = [1, 2, 3, 4, 5]
+
+# for val in nums:
+#     print(val)
+    
+# veggies  = ["tomato","potato", 2, 3, 4, 5]
+
+# for val in veggies:
+#     print(val)
+
+#using for loop
+
+# nums =[1,4,9,16,25,36,49,64,81,100]
+
+# for idx in nums:
+#     print(idx)
+#     idx+=1
+
+nums =[1,4,9,16,25,36,49,64,81,100]
+
+x = 64
+idx = 0
+for el in nums:
+    if (el ==x):
+        print ("found at index:", idx)
+        
+        break
+    idx +=1
+    
