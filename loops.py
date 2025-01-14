27d0444609971ecbabefd007fe4e75ba4f06cc2e
pallavi23102003 <120883959+pallavi23102003@users.noreply.github.com>
2025-01-14 14:32:11 +0530

    updated

--- loops.py
+++ loops.py
@@ -26,5 +26,12 @@
 #     print(nums[idx])
 #     idx+=1
 
-nums =(1,4,9,16,25,36,49,64,81,100)
-while mzvxcja
+# nums =(1,4,9,16,25,36,49,64,81,100)
+
+# x = 49
+
+# i = 0 
+# while i <len(nums):
+#     if(nums[i] == x):
+#         print("Found at index:", i)
+#     i += 1
