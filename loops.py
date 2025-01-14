33889e5219d1ef48417348e1db6406f756137a93
pallavi23102003 <120883959+pallavi23102003@users.noreply.github.com>
2025-01-14 14:19:20 +0530

    changed

--- loops.py
+++ loops.py
@@ -1,4 +1,30 @@
-count = 1
-while count <=5:
-    print("hello")
-    count += 1
+# count = 1
+# while count <=5:
+#     print("hello")
+#     count += 1
+    
+# i= 1
+# while i<=100 :
+#     print(i)
+#     i+=1
+
+# i= 100
+# while i>=1 :
+#     print(i)
+#     i-=1
+
+# n= int(input("enter a number:"))
+# i = 1
+# while i<= 10:
+#     print(n*i)
+#     i+=1
+
+# nums =[1,4,9,16,25,36,49,64,81,100]
+
+# idx = 0 
+# while idx <= len(nums)-1:
+#     print(nums[idx])
+#     idx+=1
+
+nums =(1,4,9,16,25,36,49,64,81,100)
+while 
