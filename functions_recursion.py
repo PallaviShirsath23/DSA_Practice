a1e17702cda65fdf6dc06a3e3ba91d9aef4ef538
pallavi23102003 <120883959+pallavi23102003@users.noreply.github.com>
2025-01-15 22:08:26 +0530

    new

---
+++ functions_recursion.py
@@ -0,0 +1,44 @@
+# a = 5
+# b = 10
+# sum = a + b
+# print (sum)
+
+# a = 3
+# b = 10
+# sum = a + b
+# print (sum)
+
+# a = 2
+# b = 30
+# sum = a + b
+# print (sum)
+
+# def calc_sum(a,b):
+#     sum = a+b
+#     print(sum)
+#     return sum
+
+#average of three numbers
+
+# a = int(input("Enter the first number:"))
+# b = int(input("Enter the second number:"))
+# c = int(input("Enter the third number:"))
+
+# def average():
+#     a = int(input("Enter the first number:"))
+#     b = int(input("Enter the second number:"))
+#     c = int(input("Enter the third number:"))
+
+#     sum = a+b+c
+#     avg = sum/3
+#     print("The average of the three numbers is:",avg)
+#     return average
+
+# average()
+
+
+ 
+
+
+
+
