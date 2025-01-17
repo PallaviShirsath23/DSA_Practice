916796df7fce33f3f0585fc122a5db61550055b7
pallavi23102003 <120883959+pallavi23102003@users.noreply.github.com>
2025-01-17 21:20:59 +0530

    updated

--- File_io.py
+++ File_io.py
@@ -1,1 +1,17 @@
-open()
+f = open("demo.txt","r")
+# data = f.read()
+# print(data)
+# print(type(data))
+# f.close()  
+
+data = f.read()
+print(data)
+
+line1 = f.readline()
+print(line1)
+
+line2 = f.readline()
+print(line2)
+
+
+f.close()
