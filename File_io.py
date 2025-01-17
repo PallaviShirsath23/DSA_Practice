2cd03f5a3dd224b01981ee5c22b8cbfcc68029ab
pallavi23102003 <120883959+pallavi23102003@users.noreply.github.com>
2025-01-17 23:14:18 +0530

    updated

--- File_io.py
+++ File_io.py
@@ -1,17 +1,38 @@
-f = open("demo.txt","r")
+# f = open("demo.txt","r")
 # data = f.read()
 # print(data)
 # print(type(data))
 # f.close()  
 
-data = f.read()
-print(data)
+# data = f.read()
+# print(data)
 
-line1 = f.readline()
-print(line1)
+# line1 = f.readline()
+# print(line1)
 
-line2 = f.readline()
-print(line2)
+# line2 = f.readline()
+# print(line2)
+
+# f = open("demo.txt","w")
+# f.write("this is a new file") #overwrites the entire file
+
+# f = open("demo.txt","a")
+# f.write("this is a new line") #adds to the file
 
 
+# f.close()
+
+# f = open("demo.txt","r+")
+# f.write("abc")
+# print(f.read())
+# f.close()
+
+# f = open("demo.txt","w+")
+# f.write("abc")
+# print(f.read())
+# f.close()
+
+f = open("demo.txt","w+")
+f.write("abc")
+print(f.read())
 f.close()
